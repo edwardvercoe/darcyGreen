7c9148f346f13c3c24dcb124bdc26fc59a90b54a
Edward Vercoe <Eddie@192-168-1-102.tpgi.com.au>
2018-12-06 18:08:57 +1100

    ios double click fix
    added jquery for attempted fix

--- js/index.js
+++ js/index.js
@@ -9,6 +9,16 @@ $('a').click(function(e){
     }, 1000);
 });
 
+*/
+
+$(document).ready(function() {
+  $("a").on("click touchend", function(e) {
+    var el = $(this);
+    var link = el.attr("href");
+    window.location = link;
+  });
+});
+
 /* ------------------------------------------
   NAV END
 --------------------------------------------- */
